c7ff06ba1f278c70985852232a10ce19a98530ec
keypang <bestnest820@naver.com>
2025-04-29 22:10:19 +0900

    [#26] chore: 테스트 코드 삭제

--- src/Main.java
+++ src/Main.java
@@ -12,29 +12,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*GameManager gameManager = new GameManager();
+        GameManager gameManager = new GameManager();
         GameConfigView view = new SwingConfigScreen();
         StartScreenController startController = new StartScreenController(view, gameManager);
 
         // GameScreenController를 StartScreenController에서 접근할 수 있도록 제공
-        startController.setGameManger(gameManager);*/
-
-        //테스트 코드 siyoon
-
-        Player player1 = new Player(1,4);
-        Player player2 = new Player(2,4);
-        ArrayList<Player> players = new ArrayList<>();
-        players.add(player1);
-        players.add(player2);
-        Board board = new Board(players, BoardType.SQUARE);
-        int[] moves = new int[3];
-        moves[0] = 1;
-        moves[1] = 4;
-        moves[2] = 5;
-        int[] movea = board.getMovableCells(board.getCell(5),moves);
-
-        for (int j : movea) {
-            System.out.println(j);
-        }
+        startController.setGameManger(gameManager);
     }
 }
